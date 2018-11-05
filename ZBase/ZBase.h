058638aece3c29a3decb20bd46dd1117d6b6c52b
Han <tank2308635@163.com>
2018-11-05 14:45:54 +0800

    init

---
+++ ZBase/ZBase.h
@@ -0,0 +1,19 @@
+//
+//  ZBase.h
+//  ZBase
+//
+//  Created by hsj on 2018/11/5.
+//  Copyright © 2018年 hsj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZBase.
+FOUNDATION_EXPORT double ZBaseVersionNumber;
+
+//! Project version string for ZBase.
+FOUNDATION_EXPORT const unsigned char ZBaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZBase/PublicHeader.h>
+
+
